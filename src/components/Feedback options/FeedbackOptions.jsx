6fba32f68f4bd45be6619ef390d83ea8b6d69249
julgrabar <45499113+julgrabar@users.noreply.github.com>
2022-04-11 09:55:23 +0300

    Update FeedbackOptions.jsx

--- src/components/Feedback options/FeedbackOptions.jsx
+++ src/components/Feedback options/FeedbackOptions.jsx
@@ -7,5 +7,5 @@ export const FeedbackOptions = ({options, onLeaveFeedback}) => (
 
 FeedbackOptions.propTypes ={
     options: PropTypes.arrayOf(PropTypes.string).isRequired,
-    onLeaveFeedback: PropTypes.func
+    onLeaveFeedback: PropTypes.func.isRequired
 }
